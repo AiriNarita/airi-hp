d1d5fff98c08d46fef79e2f8a9c3502506607f3e
成田愛里 <airiswim.kitty@gmail.com>
2025-02-04 23:57:36 +0900

    feat: blog detail page

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+
+export default function BlogDetailPage() {
+    return  <>
+     <main className="container mx-auto px-4 py-20">
+        <div className="absolute inset-0 bg-black bg-opacity-50">
+        </div>
+    </main>
+    
+    </>
