2bf5e672ba23fad6d8e7466d7e0767b9756e77ab
成田愛里 <airiswim.kitty@gmail.com>
2025-03-02 10:53:21 +0900

    feat:  dependency 修正

--- app/components/Zenn/Zenn.tsx
+++ app/components/Zenn/Zenn.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { ArticleList } from '../ui/ArticleCard';
+
 
 interface ZennArticle {
   title: string;
--- app/i18n/client.tsx
+++ app/i18n/client.tsx
@@ -12,6 +12,7 @@ import {
   initReactI18next,
   useTranslation as useTranslationOrigin,
 } from 'react-i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
 
 import { getOptions } from './settings';
 
